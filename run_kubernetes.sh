285ebfa2a2c04fae4c823f6a674a0a47904e1f7d
unknown <md.imranur.rahman@grameenphone.com>
2020-05-21 16:17:33 +0600

    changes in code

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=rahmanimran/cfit-machine-learning
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment cfit-machine-learning-cluster --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl proxy
+export POD_NAME=$(kubectl get pods -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
+echo Name of the Pod: $POD_NAME
